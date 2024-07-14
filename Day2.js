9d6e4c2ebbfc412f974c9c04564315b2546c50c3
RakeshDhiwar <dhiwarrakesh43@gmail.com>
2024-07-14 23:25:35 +0530

    Day 2 Challenge Completed on Operator

---
+++ Day2.js
@@ -0,0 +1,56 @@
+// Day 2: Operators
+    //Activity 1: Arithmetic Operations
+        //Task 1: WAP to add two numbers and log the result to the console
+        let num1 = 8;
+        let num2 = 8;
+        console.log(num1 + num2);
+        //Task 2: WAP to subtract two numbers and log the result to the console
+        let num3 = 8;
+        let num4 = 8;
+        console.log(num1 - num2);
+        //Task 3: WAP to multiply two numbers and log the result to the console
+        let num5 = 8;
+        let num6 = 8;
+        console.log(num1 * num2);
+        //Task 4: WAP to add two divide and log the result to the console
+        let num7 = 8;
+        let num8 = 8;
+        console.log(num1 / num2);
+        //Task 5: WAP to find the remainder when one number is divided by another and log the result
+        let num9 = 8;
+        let num0 = 8;
+        console.log(num1 % num2);
+
+    //Activity 2: Assignment Operators
+        //Task 6: Use the += operator to add a number to a variable and log the result to the console.
+        let num11 = 8;
+        num11 += 8;
+        console.log(num11);
+        //Task 7: Use the -= operator to subtract a number to a variable and log the result to the console.
+        let num12 = 8;
+        num12 += 8;
+        console.log(num12);
+    
+    //Activity 3: Comparision Operators
+        //Task 8: WAP to compare two numbers using > and < and log the result to the console.
+        console.log(num1 > num0);
+        //Task 9: WAP to compare two numbers using >= and <- and log the result to the console.
+        console.log(num1 < num0);
+        //Task 10: WAP to compare two numbers using == and === and log the result to the console.
+        let str1 = "1"; //string
+        let str2 = 1;   //number
+        console.log(str1 == str2); //true
+        console.log(str1 === str2);//false
+
+    //Activity 4: Logical Operators
+        //Task 11: WAP that uses && opearator to combine two conditions and log the result to the console.
+        console.log((2>3)&&(3>2));
+        //Task 11: WAP that uses || opearator to combine two conditions and log the result to the console.
+        console.log((2>3)||(3>2));
+        //Task 13: WAP that uses the ! operator to negate a condition and log the result to the console.
+        console.log(!((2>3)||(3>2)));
+    
+    //Activity 5: Ternary Operator
+        //Task 14: WAP that useu the ternary operator to check if a  number is positive or negative and log the result to the console.
+        let number = 8;
+        (number > 0) ?console.log("Positive") : console.log("negative");
